bb9ca1716dd916ac1c52c73a414556b3234e4e7d
avipul49 <avipul49@gmail.com>
2015-04-19 19:34:15 -0400

    Db record service added

--- src/main/java/com/baseballanalysis/controller/RecordsController.java
+++ src/main/java/com/baseballanalysis/controller/RecordsController.java
@@ -37,6 +37,74 @@ public class RecordsController extends BaseballController {
 		return null;
 	}
 
+	@RequestMapping("/getDatabaseDetails")
+	public @ResponseBody DatabaseDetails getDatabaseDetails(
+			HttpServletResponse response) {
+
+		setResposeObject(response);
+		try {
+			Connection connection = DatabaseConnection.getConnection();
+			CallableStatement cstmt = connection
+					.prepareCall("{CALL get_database_details(?,?,?,?)}");
+			cstmt.registerOutParameter(1, Types.INTEGER);
+			cstmt.registerOutParameter(2, Types.INTEGER);
+			cstmt.registerOutParameter(3, Types.INTEGER);
+			cstmt.registerOutParameter(4, Types.INTEGER);
+
+			cstmt.executeUpdate();
+			DatabaseDetails result = new DatabaseDetails();
+			result.setRecords(cstmt.getInt(1));
+			result.setTeams(cstmt.getInt(2));
+			result.setPlayers(cstmt.getInt(3));
+			result.setManagers(cstmt.getInt(4));
+			connection.close();
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	static class DatabaseDetails {
+		private int records;
+		private int teams;
+		private int players;
+		private int managers;
+
+		public int getRecords() {
+			return records;
+		}
+
+		public void setRecords(int records) {
+			this.records = records;
+		}
+
+		public int getTeams() {
+			return teams;
+		}
+
+		public void setTeams(int teams) {
+			this.teams = teams;
+		}
+
+		public int getPlayers() {
+			return players;
+		}
+
+		public void setPlayers(int players) {
+			this.players = players;
+		}
+
+		public int getManagers() {
+			return managers;
+		}
+
+		public void setManagers(int managers) {
+			this.managers = managers;
+		}
+
+	}
+
 	static class Result {
 		private String record;
 
